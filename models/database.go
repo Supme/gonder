debc9f81b5fe96ad7afc3415642afb6387e56f56
supme <supmea@gmail.com>
2019-11-11 12:06:24 +0300

    right check version

--- models/database.go
+++ models/database.go
@@ -44,7 +44,7 @@ func CheckDb() error {
 	}
 
 	// in the future, here, if necessary, will check the version of the application and the database
-	if version.Compare(Version, dbVersion, ">") {
+	if version.Compare(Version, dbVersion, "<") {
 		if err = dbQueryFrom("sql/update/0.16.3.sql"); err != nil {
 			return fmt.Errorf("update database to version %s: %s", Version, err)
 		}
